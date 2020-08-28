5b2fd7b57961a32c3c24dd8a5a85319362253728
Talgat Muratbekov <talgat2307@gmail.com>
2020-08-29 03:18:16 +0600

    #4 - Updated App.js and App.css

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {Route, Switch, BrowserRouter} from 'react-router-dom';
+import Home from './containers/Home/Home';
+import Add from './containers/Add/Add';
+import About from './containers/About/About';
+import Contacts from './containers/Contacts/Contacts';
+import Header from './containers/Header';
+import SinglePost from './containers/SinglePost/SinglePost';
+import Edit from './containers/Edit/Edit';
 
-function App() {
+const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <Header/>
+        <Switch>
+          <Route path='/' exact component={Home} />
+          <Route path='/posts/add' component={Add}/>
+          <Route path='/about' component={About} />
+          <Route path='/contacts' component={Contacts} />
+          <Route path='/posts/:id/edit' component={Edit} />
+          <Route path='/posts/:id' component={SinglePost} />
+          <Route path='/posts' component={Home} />
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
